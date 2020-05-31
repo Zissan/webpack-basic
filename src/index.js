e45f9a0f2111f7dc3031dd3223c4d8eb32f4c7bc
zissan chowdhury <zissan.chowdhury@yahoo.com>
2020-05-31 17:14:31 +0530

    1. Added url-loader into devDependencies. It helps load images from the server.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from "react";
 import { render } from "react-dom";
 import "./index.css";
 
-const Greeting = () => <h1>Webpack, getting started using React</h1>;
+const Greeting = () => {
+  return (
+    <div>
+      <h1>Webpack, getting started using React</h1>
+      <div className="image"></div>
+    </div>
+  );
+};
 
 render(<Greeting />, document.getElementById("mount"));
